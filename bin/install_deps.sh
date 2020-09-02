1bdbb216b72a9909499697aaea3f4541f00c5305
nekoffski <mrnek135@gmail.com>
2020-09-02 21:28:10 +0200

    removed unused submodules

--- bin/install_deps.sh
+++ bin/install_deps.sh
@@ -12,4 +12,5 @@ sudo apt-get install -y libgl-dev
 sudo apt-get install -y uuid-dev
 sudo apt-get install -y libassimp-dev
 sudo apt-get install -y libglfw3-dev
+sudo apt-get install -y libjsoncpp-dev
 
