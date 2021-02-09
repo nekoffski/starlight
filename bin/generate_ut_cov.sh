00c76e0c2c6e002742bc1f43e3e7f0f5371900c9
nekoffski <kacperczyz@protonmail.com>
2021-02-09 22:59:36 +0100

    fix

--- bin/generate_ut_cov.sh
+++ bin/generate_ut_cov.sh
@@ -5,6 +5,8 @@ sudo python3.7 -m pip install gcovr
 
 cd cov_build
 
+ls /usr/bin | grep gcov
+
 python3.7 -m gcovr --version
 python3.7 -m gcovr -r . -f ../src --gcov-executable /usr/bin/gcov-10 --exclude-throw-branches --exclude-unreachable-branches --xml-pretty --verbose --output ../coverage.xml
 
