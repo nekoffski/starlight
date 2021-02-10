192b34cc3a26f00c1a398b5e9ca8d42fafb492b6
nekoffski <kacperczyz@protonmail.com>
2021-02-10 19:44:18 +0100

    Fix

--- bin/generate_ut_cov.sh
+++ bin/generate_ut_cov.sh
@@ -8,6 +8,6 @@ cd cov_build
 ls /usr/bin | grep gcov
 
 python3.7 -m gcovr --version
-python3.7 -m gcovr -r . -f ../src --gcov-executable /usr/bin/gcov-10 --exclude-throw-branches --exclude-unreachable-branches --xml-pretty --verbose --output ../coverage.xml
+python3.7 -m gcovr -r . -f ../src --gcov-executable /usr/bin/gcov-10 --exclude-throw-branches --xml-pretty --output ../coverage.xml
 
 cat ../coverage.xml
