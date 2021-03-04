45e6282ffd6258c033823269b0a393130a8d13a7
nekoffski <kacperczyz@protonmail.com>
2021-03-04 19:39:04 +0100

    ff

--- bin/install_deps.sh
+++ bin/install_deps.sh
@@ -14,4 +14,4 @@ sudo apt-get install -y libassimp-dev
 sudo apt-get install -y libglfw3-dev
 sudo apt-get install -y libjsoncpp-dev
 sudo apt-get install -y libglm-dev
-sudo apt-get install -y python3.7
+
