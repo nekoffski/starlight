9e39b228d69580c8d8079edfa8f389f6748e5dc4
nekoffski <kacperczyz@protonmail.com>
2021-09-27 20:32:34 +0200

    Update libkc

--- src/sl/cfg/ConfigLoader.cpp
+++ src/sl/cfg/ConfigLoader.cpp
@@ -11,22 +11,22 @@ void ConfigLoader::processFields(const kc::json::Node& root) {
 
     LOG_DEBUG("Processing config fields");
     LOG_TRACE("Processing paths config");
-    auto paths = fieldFrom(root).ofName("paths").asObject().get();
+    auto paths = fieldFrom(root).withName("paths").asObject().get();
 
     // clang-format off
-    config().paths.cubemaps = fieldFrom(paths).ofName("cubemaps").ofType<std::string>().nonEmpty().get();
-    config().paths.models   = fieldFrom(paths).ofName("models")  .ofType<std::string>().nonEmpty().get();
-    config().paths.shaders  = fieldFrom(paths).ofName("shaders") .ofType<std::string>().nonEmpty().get();
-    config().paths.textures = fieldFrom(paths).ofName("textures").ofType<std::string>().nonEmpty().get();
+    config().paths.cubemaps = fieldFrom(paths).withName("cubemaps").ofType<std::string>().nonEmpty().get();
+    config().paths.models   = fieldFrom(paths).withName("models")  .ofType<std::string>().nonEmpty().get();
+    config().paths.shaders  = fieldFrom(paths).withName("shaders") .ofType<std::string>().nonEmpty().get();
+    config().paths.textures = fieldFrom(paths).withName("textures").ofType<std::string>().nonEmpty().get();
     // clang-format on
 
     LOG_TRACE("Processing window config");
-    auto window = fieldFrom(root).ofName("window").asObject().get();
+    auto window = fieldFrom(root).withName("window").asObject().get();
 
     // clang-format off
-    config().window.height = fieldFrom(window).ofName("height").ofType<unsigned int>().get();
-    config().window.width  = fieldFrom(window).ofName("width") .ofType<unsigned int>().get();
-    config().window.name   = fieldFrom(window).ofName("name") .ofType<std::string>()  .get();
+    config().window.height = fieldFrom(window).withName("height").ofType<unsigned int>().get();
+    config().window.width  = fieldFrom(window).withName("width") .ofType<unsigned int>().get();
+    config().window.name   = fieldFrom(window).withName("name") .ofType<std::string>()  .get();
     // clang-format on
 
     LOG_DEBUG("Config processed successfully");
