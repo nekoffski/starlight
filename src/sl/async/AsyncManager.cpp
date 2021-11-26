350aba4445a242c427961da2aad51af13e596c5f
nekoffski <kacperczyz@protonmail.com>
2021-11-26 19:52:41 +0100

    Remove commented out code

--- src/sl/async/AsyncManager.cpp
+++ src/sl/async/AsyncManager.cpp
@@ -4,41 +4,13 @@
 
 namespace sl::async {
 
-AsyncManager::AsyncManager() {
-    m_threadPool = std::make_unique<kc::async::ThreadPool>(4);
+AsyncManager::AsyncManager()
+    : m_threadPool(std::make_unique<kc::async::ThreadPool>(4)) {
 }
 
 AsyncManager::~AsyncManager() {
     m_threadPool->stop();
 }
-
-// // TODO: replace std::function with template
-// void AsyncManager::parallelLoop(const int iterations, const std::function<void(const int)>& func) {
-// auto threadCount = m_threadPool->getSize();
-// auto batch = iterations / threadCount;
-
-// std::vector<kc::async::Future<void>> futures;
-// futures.reserve(threadCount);
-
-// for (int i = 0; i < threadCount; ++i) {
-//     int beg = i * batch;
-//     int end = (i + 1) * batch;
-
-//     if (i == threadCount - 1)
-//         end += iterations % batch;
-
-//     // clang-format off
-//     futures.emplace_back(
-//         m_threadPool->callAsync([func](int beg, int end) {
-//             for (int i = beg; i < end; ++i)
-//                 func(i);
-//         }, beg, end));
-//     // clang-format on
-// }
-
-// for (auto& future : futures)
-//     future.wait();
-// }
 
 void AsyncManager::update(float dtime) {
     processTimers(dtime);
