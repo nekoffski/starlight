4cf837a239fa304e13797127623a13917478ba3f
nekoffski <kacperczyz@protonmail.com>
2022-05-11 07:22:51 +0200

    fix path

--- bin/generate_ut_cov.sh
+++ bin/generate_ut_cov.sh
@@ -8,6 +8,6 @@ cd cov_build
 ls /usr/bin | grep gcov
 
 python3 -m gcovr --version
-python3 -m gcovr -r . -f ../engine -f ../editor --gcov-executable /usr/bin/gcov-10 --exclude-throw-branches --xml-pretty --output ../coverage.xml
+python3 -m gcovr -r . -f ../engine/src -f ../editor --gcov-executable /usr/bin/gcov-10 --exclude-throw-branches --xml-pretty --output ../coverage.xml
 
 cat ../coverage.xml
