7915ed71d893a61b02b129eb387249745763c6a9
nekoffski <kacperczyz@protonmail.com>
2022-05-25 22:50:02 +0200

    add libvulkan to deps

--- bin/deps.sh
+++ bin/deps.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y gcc-10 g++-10
 sudo apt-get install -y libxrandr-dev libxinerama-dev libx11-dev libxcursor-dev libxi-dev libgl-dev \
-    uuid-dev libassimp-dev libglfw3-dev libjsoncpp-dev libglm-dev libgtest-dev libgmock-dev libfmt-dev vulkansdk
+    uuid-dev libassimp-dev libglfw3-dev libjsoncpp-dev libglm-dev libgtest-dev libgmock-dev libfmt-dev libvulkan-den
 
 if [ -d ./3rdparty/libkc ]; then
     chmod +x ./3rdparty/libkc/bin/deps.sh
