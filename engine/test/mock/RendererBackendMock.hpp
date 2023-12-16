5c665d79efa1f0a391162d14006ab3c09816ed39
nekoffski <kacperczyz@protonmail.com>
2023-12-16 01:05:19 +0100

    Fix test build

--- engine/test/mock/RendererBackendMock.hpp
+++ engine/test/mock/RendererBackendMock.hpp
@@ -9,10 +9,7 @@ using namespace sl;
 struct RendererBackendMock : RendererBackend {
     MOCK_METHOD(bool, beginFrame, (float));
     MOCK_METHOD(bool, endFrame, (float));
-    MOCK_METHOD(bool, beginRenderPass, (uint8_t));
-    MOCK_METHOD(u64, endRenderPass, (uint8_t));
-    MOCK_METHOD(void, renderUI, (std::function<void()> &&));
-    MOCK_METHOD(void, drawGeometry, (const GeometryRenderData&));
+    MOCK_METHOD(void, drawGeometry, (const Geometry&));
     MOCK_METHOD(void, onViewportResize, (uint32_t, uint32_t));
     MOCK_METHOD(ResourcePools*, getResourcePools, ());
     MOCK_METHOD(RendererBackendProxy*, getProxy, ());
