0c428d1e7fe32f53e8fe56fbd7abec7c7599a5ee
nekoffski <kacperczyz@protonmail.com>
2024-08-21 23:03:08 +0200

    fix

--- bin/deps.sh
+++ bin/deps.sh
@@ -7,10 +7,26 @@ if [ -d ./3rdparty/libkc ]; then
     ./3rdparty/libkc/bin/deps.sh
 fi;
 
-sudo apt-get install -y gcc-10 g++-10
-sudo apt-get install -y libxrandr-dev libxinerama-dev libx11-dev libxcursor-dev libxi-dev libgl-dev \
-    uuid-dev libassimp-dev libglfw3-dev libjsoncpp-dev libglm-dev libgtest-dev libgmock-dev libfmt-dev libassimp-dev \
-    libfreetype-dev cppcheck libbost-dev
-    
+sudo apt-get install -y gcc-10 
+sudo apt-get install -y g++-10
+sudo apt-get install -y libxrandr-dev 
+sudo apt-get install -y libxinerama-dev 
+sudo apt-get install -y libx11-dev 
+sudo apt-get install -y libxcursor-dev 
+sudo apt-get install -y libxi-dev 
+sudo apt-get install -y libgl-dev 
+sudo apt-get install -y uuid-dev 
+sudo apt-get install -y libassimp-dev 
+sudo apt-get install -y libglfw3-dev 
+sudo apt-get install -y libjsoncpp-dev 
+sudo apt-get install -y libglm-dev 
+sudo apt-get install -y libgtest-dev 
+sudo apt-get install -y libgmock-dev 
+sudo apt-get install -y libfmt-dev 
+sudo apt-get install -y libassimp-dev
+sudo apt-get install -y libfreetype-dev 
+sudo apt-get install -y cppcheck 
+sudo apt-get install -y libbost-dev
+
 # show vulkan version
 apt list --installed | grep vulkan
