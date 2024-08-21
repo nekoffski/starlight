4853eeeec963abf7a39dfc8085784b4ec4b4936a
nekoffski <kacperczyz@protonmail.com>
2024-08-21 22:40:19 +0200

    fix

--- bin/deps.sh
+++ bin/deps.sh
@@ -14,8 +14,8 @@ sudo apt-get install -y libxrandr-dev libxinerama-dev libx11-dev libxcursor-dev 
 
 wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
 sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-noble.list http://packages.lunarg.com/vulkan/lunarg-vulkan-noble.list
-sudo apt update
-sudo apt install vulkan-sdk
+sudo apt-get update
+sudo apt-get install vulkan-sdk
 
 # show vulkan version
 apt list --installed | grep vulkan
