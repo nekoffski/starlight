b5524bbb398456a5313ac6d4e49f8b9a9e1a890d
nekoffski <kacperczyz@protonmail.com>
2024-08-21 22:53:20 +0200

    fix

--- bin/deps.sh
+++ bin/deps.sh
@@ -11,11 +11,6 @@ sudo apt-get install -y gcc-10 g++-10
 sudo apt-get install -y libxrandr-dev libxinerama-dev libx11-dev libxcursor-dev libxi-dev libgl-dev \
     uuid-dev libassimp-dev libglfw3-dev libjsoncpp-dev libglm-dev libgtest-dev libgmock-dev libfmt-dev libassimp-dev \
     libfreetype-dev cppcheck libbost-dev
-
-wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
-sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-noble.list http://packages.lunarg.com/vulkan/lunarg-vulkan-noble.list
-sudo apt-get update
-sudo apt-get install vulkan-sdk
-
+    
 # show vulkan version
 apt list --installed | grep vulkan
