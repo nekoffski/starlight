3854cff2609be2c4e6603bab9d97ed74d14ded25
nekoffski <kacperczyz@protonmail.com>
2024-10-16 20:36:52 +0200

    Remove unused "resource" component

--- engine/src/starlight/resource/All.hh
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "managers/MaterialManager.hh"
-#include "managers/MeshManager.hh"
-#include "managers/ShaderManager.hh"
-#include "managers/TextureManager.hh"
-#include "managers/SkyboxManager.hh"
-#include "managers/ModelManager.hh"
