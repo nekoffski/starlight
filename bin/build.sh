d8c1d37e8f2e8361e6a0cd57d6f29e86c8c1ee19
nekoffski <kacperczyz@protonmail.com>
2024-10-23 22:41:59 +0200

    dbg

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,11 @@ fi;
 
 conan install ./conan/ --output-folder=../build --build=missing --profile ./conan/profiles/debug
 
+ls -al
+
 cd build
+
+ls -al
 
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
 cmake --build . -j8
