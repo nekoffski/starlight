a5887c54a3554f73db88930ca1e23eb33cb100d1
nekoffski <kacperczyz@protonmail.com>
2024-10-23 22:45:00 +0200

    fix

--- bin/build.sh
+++ bin/build.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-if ! [ -d ./build ]; then
-    mkdir ./build
-fi;
 
-conan install ./conan/ --output-folder=../build --build=missing --profile ./conan/profiles/debug
-
-ls -al
+conan install ./conan/ --output-folder=./build --build=missing --profile ./conan/profiles/debug
 
 cd build
-
-ls -al
 
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
 cmake --build . -j8
