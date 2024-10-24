7080dcd50f7eadacb56319a929c1322cd84d6399
nekoffski <kacperczyz@protonmail.com>
2024-10-24 19:21:00 +0200

    coverage

--- bin/generate-ut-cov.sh
+++ bin/generate-ut-cov.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# sudo apt-get install python3
-# sudo python3 -m pip install gcovr
 
-# cd cov_build
-
-# ls /usr/bin | grep gcov
-
-# python3 -m gcovr --version
-# python3 -m gcovr -r . -f ../engine/src -f ../editor --gcov-executable /usr/bin/gcov-10 --exclude-throw-branches --xml-pretty --output ../coverage.xml
-
-# cat ../coverage.xml
+cd build
+source ../venv/bin/activate
+gcovr -f ../engine/src --exclude-throw-branches --html --output ../coverage.html
