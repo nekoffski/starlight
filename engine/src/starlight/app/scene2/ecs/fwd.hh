34b793fbc87baa07d670fb52f4148a6caec9720d
Kacper Czyz <kacperczyz@protonmail.com>
2025-03-05 23:44:12 +0100

    Remove scene2

--- engine/src/starlight/app/scene2/ecs/fwd.hh
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace sl::v2 {
-
-class Entity;
-class EntityManager;
-
-}  // namespace sl::v2
--- engine/test/src/Tests.App.Scene.Ecs.Component.cpp
+++
@@ -1,4 +0,0 @@
-#include "starlight/app/scene2/ecs/Component.hh"
-
-// struct TestComponent : public sl::v2::Component<TestComponent, "TestComponent">
-// {};
